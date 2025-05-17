053621d1e94058fabfbd405c778bf7e3ba73c5c8
msaroshali <muhammadsaroshali@gmail.com>
2025-05-17 21:47:27 +0200

    minor changes

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../page-Objects/login-page';
 import { HomePage } from '../page-Objects/home-page';
 import signUpData from '../test-data/signUpData.json';
-import loginData from '../test-data/loginData.json';
 
 const MailosaurClient = require("mailosaur");
 
@@ -11,18 +10,17 @@ test('Sign Up', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const homepage = new HomePage(page);
   
-  // // Initializing the Mailosaur client with API key
+  // Initializing the Mailosaur client with API key
   const mailosaur = new MailosaurClient(signUpData.apiKey);
 
-  // // Email address for the test
+  // Email address for the test
   const emailAddress = mailosaur.servers.generateEmailAddress(signUpData.serverID);
 
-  // // Navigate to the login page and get started
+  // Navigate to the login page and get started
    await loginPage.goto();
    await loginPage.acceptCookies();
 
   // Verify the login page 
-  
   await loginPage.verifyLoginPageVisible();
 
   //Begin the signup process
@@ -88,8 +86,8 @@ test('Sign Up', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).fill(signUpData.password);
   await page.getByRole('button', { name: 'Sign in' }).click();
 
-await expect(page.getByText('We use cookies to personalise')).toBeVisible();
-await page.getByRole('link', { name: 'Yes' }).click({ timeout: 60000 });
+  await expect(page.getByText('We use cookies to personalise')).toBeVisible();
+  await page.getByRole('link', { name: 'Yes' }).click({ timeout: 60000 });
   //Complete remaining steps 
   await expect(page.locator('body')).toContainText('Complete Your Profile');
   await expect(page.locator('div').filter({ hasText: 'First Name *Last Name *Are' }).nth(1)).toBeVisible();
@@ -108,39 +106,39 @@ await page.getByRole('link', { name: 'Yes' }).click({ timeout: 60000 });
   await expect(page.locator('body')).toContainText('Organization Name *');
   await expect(page.locator('body')).toContainText('Organization Size *');
 
-await expect(page.locator('body')).toContainText('Business Role *');
-await expect(page.locator('body')).toContainText('Your Role *');
-await expect(page.locator('body')).toContainText('Main Use Cases *');
-await expect(page.locator('body')).toContainText('How did you first learn about Maltego?');
+  await expect(page.locator('body')).toContainText('Business Role *');
+  await expect(page.locator('body')).toContainText('Your Role *');
+  await expect(page.locator('body')).toContainText('Main Use Cases *');
+  await expect(page.locator('body')).toContainText('How did you first learn about Maltego?');
 
-await expect(page.getByText('Focus of Business Unit *')).toBeVisible();
-await page.getByRole('textbox', { name: 'Organization Name' }).click();
-await page.getByRole('textbox', { name: 'Organization Name' }).fill(signUpData.orgName);
+  await expect(page.getByText('Focus of Business Unit *')).toBeVisible();
+  await page.getByRole('textbox', { name: 'Organization Name' }).click();
+  await page.getByRole('textbox', { name: 'Organization Name' }).fill(signUpData.orgName);
 
-await page.getByRole('combobox', { name: 'Organization Size' }).click();
-await page.getByRole('option', { name: '1-10', exact: true }).click();
-await page.getByRole('textbox', { name: 'Your Role' }).click();
-await page.getByRole('textbox', { name: 'Your Role' }).fill(signUpData.role);
+  await page.getByRole('combobox', { name: 'Organization Size' }).click();
+  await page.getByRole('option', { name: '1-10', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Your Role' }).click();
+  await page.getByRole('textbox', { name: 'Your Role' }).fill(signUpData.role);
 
-await page.getByRole('textbox', { name: 'Organization website' }).click();
-await page.getByRole('combobox', { name: 'How did you first learn about' }).click();
-await page.getByRole('option', { name: 'Social Media' }).click();
+  await page.getByRole('textbox', { name: 'Organization website' }).click();
+  await page.getByRole('combobox', { name: 'How did you first learn about' }).click();
+  await page.getByRole('option', { name: 'Social Media' }).click();
 
-await page.getByRole('combobox', { name: 'Focus of Business Unit' }).click();
-await page.getByRole('option', { name: 'Trust & Safety' }).click();
-await page.locator('.MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click();
-await page.getByRole('option', { name: 'Incident Response' }).click();
-await page.getByRole('button', { name: 'Close' }).click();
-await expect(page.locator('body')).toContainText('Continue');
-await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('combobox', { name: 'Focus of Business Unit' }).click();
+  await page.getByRole('option', { name: 'Trust & Safety' }).click();
+  await page.locator('.MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click();
+  await page.getByRole('option', { name: 'Incident Response' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await expect(page.locator('body')).toContainText('Continue');
+  await page.getByRole('button', { name: 'Continue' }).click();
 
-await page.goto('https://app.maltego.com/');
-await expect(page.getByRole('heading')).toContainText('Sign In to Maltego');
-await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible();
-await expect(page.getByRole('button', { name: 'Sign in' })).toBeVisible();
-await page.getByRole('textbox', { name: 'Password' }).click();
-await page.getByRole('textbox', { name: 'Password' }).fill(signUpData.password);
-page.getByRole('button', { name: 'Sign in' }).click();
+  await page.goto('https://app.maltego.com/');
+  await expect(page.getByRole('heading')).toContainText('Sign In to Maltego');
+  await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Sign in' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(signUpData.password);
+  page.getByRole('button', { name: 'Sign in' }).click();
 
   // Login to the application and land on the home page
   await loginPage.loginDuringSignUp(signUpData.password);
